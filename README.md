f1c2c777ffae95287c025b8aeed14b52a6e6d6fb
Rosemarie <rosemarie.m.halim@gmail.com>
2022-03-18 20:50:11 -0400

    completed adding base global appends and attributes, making notes for next step of setting up

---
+++ README.md
@@ -0,0 +1,49 @@
+#   Challenge 04 - Quiz
+
+##  User Story
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+## Acceptance Criteria
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+---
+
+# Quiz
+
+## Purpose
+As a developer, I want to create a quiz where users can quiz themselves with a timer that depreciates in time when selecting a wrong answer and are provided a highscore that they can see afterwards.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://rosemariehalim.github.io/04-challenge/
+
+---
+
+## Challenge 3.0
+
+* planned out code
+* linked css and js to HTML file
+* created develop branch
+* created password length function with while loop statement
+* added the functions and boolians for the lowercase, uppercase, numeric, and/or special characters
+* created password generation function that provides a warning pop-up when neither boolian is true
+* tested and fixed broken code
+* merged develop branch into main and pushed to GitHub
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 // Start button to begin countdown and 4/5 sets of questions with true false buttons, will output when wrong/right
-
-
-
-
+// end of timer will show highscore and will ask to submit initials
+// go to highscores that show list of scores and initials along with two buttons, 'go back' and 'clear high scores'
 
 // ANSWERS
 var body = document.body;
 var h1El = document.createElement("h1");
-var favoriteEl = document.createElement("div");
+var answersEl = document.createElement("div");
 // ordered list element
 var listEl = document.createElement("ul");
 var li1 = document.createElement("li");
@@ -15,28 +13,29 @@ var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
 
-h1El.textContent = "Welcome to my page";
-li1.textContent = "1. Sushi";
-li2.textContent = "2. Pizza";
-li3.textContent = "3. Chocolate";
-li4.textContent = "4. Pasta";
-
 body.appendChild(h1El);
-body.appendChild(favoriteEl);
-favoriteEl.appendChild(listEl);
+body.appendChild(answersEl);
+answersEl.appendChild(listEl);
 listEl.appendChild(li1);
 listEl.appendChild(li2);
 listEl.appendChild(li3);
 listEl.appendChild(li4);
 
 h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
-favoriteEl.setAttribute("style", "display: flex; justify-content: center;")
+answersEl.setAttribute("style", "display: flex; justify-content: center;")
 listEl.setAttribute("style", "padding:20px; list-style: none; width:50%;")
-li1.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255);")
-li2.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255)")
-li3.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255)")
-li4.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255)")
+li1.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255); border-radius: 12px;")
+li2.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255); border-radius: 12px;")
+li3.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255); border-radius: 12px;")
+li4.setAttribute("style", "padding:15px; color: white; margin:10px; background-color:rgb(132, 0, 255); border-radius: 12px;")
 
+h1El.textContent = "Question";
+li1.textContent = "1. Answer";
+li2.textContent = "2. Answer";
+li3.textContent = "3. Answer";
+li4.textContent = "4. Answer";
+
+// NOTE: Create function to show questions with answers, if true = correct, else false = wrong and decrement 5s from timer, for loop to go to next question and answers
 
 // LOCAL STORAGE CODE
 // notes: make pop up to request name to input into local storage to store name and high score
@@ -56,24 +55,13 @@ signUpButton.addEventListener("click", function(event) {
 }); 
 
 
-// INCREMENT/DECREMENT FOR ANSWERS FUNCTION
+// DECREMENT FOR ANSWERS FUNCTION
 var count = 0;
 
 // locating the call for items in selector from html
 // querySelector is API that allows us to specify how we want to target elements
-var incrementEl = document.querySelector('#increment');
 var decrementEl = document.querySelector('#decrement');
 var countEl = document.querySelector('#count');
-
-// inputs content from countEl and assigning value of count variant
-function setCounterText() {
-  countEl.textContent = count;
-}
-// listening for a click on increment btn to run function and input data into countertext function to increase in value
-incrementEl.addEventListener('click', function() {
-  count++;
-  setCounterText();
-});
 
 // listening for click on decrement btn to run function and input data into countertext function to decrease in value
 decrementEl.addEventListener('click', function() {
@@ -86,27 +74,49 @@ decrementEl.addEventListener('click', function() {
 
 
 // COUNTER FUNCTION
-// regular countdown and -5 sec when wrong answer
 var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
 
 function countdown() {
   var timeLeft = 120;
 
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(() => {
 
     if (timerEl > 0){
-      timerEl.textContent = timeLeft;
+      timerEl.textContent = timeLeft + ' seconds remaining';
       // 2.0 string interpolation, template literal
       // timerEl.textContent = `${timeLeft} seconds remaining`
       timeLeft--;
     }
+    else if (timerLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+    }
     else if (timeLeft === 0) {
       timerEl.textContent = ''
       clearInterval(timeInterval);
+      displayMessage();
     }
   },
   1000); 
   }
+
+// Displays the message one word at a time
+function displayMessage() {
+  var wordCount = 0;
+
+  // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var msgInterval = setInterval(function() {
+    // If there are no more words left in the message
+    if (words[wordCount] === undefined) {
+      // Use `clearInterval()` to stop the timer
+      clearInterval(msgInterval);
+    } else {
+      // Display one word of the message
+      mainEl.textContent = words[wordCount];
+      wordCount++;
+    }
+  }, 1000);
+}
 
 countdown();
